8235cfae0848029b68b1f8c873b769fef4de9f5b
Kirill Yudov <kirill.yudov@gmail.com>
2016-11-13 01:42:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.get('/2b', function (req, res) {
 	const fullname = req.query.fullname
 	const words = validator(fullname);
   	res.send(validator(req.query.fullname));
+
+  	console.log(fullname.replace(/ {1,}/g," ").replace(/^\s*(.*)\s*$/, '$1') + ' ' + fullname);
 });
 
 app.listen(3000, function () {
@@ -35,12 +37,13 @@ function nullValidator(str){
 }
 
 function digitValidator(str) {
-        return /[-\s0-9`~!@#$%^&*()_=+\\|\[\]{};:'",.<>\/?]/;
+        return !/[-0-9~!@#$%^&*()_=+\\|\[\]{};:`",.<>\/?]/.test(str);
 }
 
 
 
 function validator(str){
+	var str = str.replace(/ {1,}/g," ").replace(/^\s*(.*)\s*$/, '$1').toLowerCase();
 	if(lengthValidator(str) && nullValidator(str) && digitValidator(str)){
 		return getFIO(str);
 	}
@@ -50,7 +53,7 @@ function validator(str){
 function getFIO(str){
 	const FIO = getWords(str);
 	//length = FIO.length;
-	if (getWords(str).length == 1) {return FIO[0];}
-	else if(getWords(str).length == 2) {return (FIO[1]  + ' ' + FIO[0].charAt(0) + '.');}
-	else {return (FIO[2]  + ' ' + FIO[0].charAt(0) + '. ' + FIO[1].charAt(0) + '.');}
+	if (getWords(str).length == 1) {return FIO[0][0].toUpperCase() + FIO[0].slice(1);}
+	else if(getWords(str).length == 2) {return (FIO[1][0].toUpperCase() + FIO[1].slice(1) + ' ' + FIO[0].charAt(0).toUpperCase() + '.');}
+	else {return (FIO[2] [0].toUpperCase() + FIO[2].slice(1) + ' ' + FIO[0].charAt(0).toUpperCase() + '. ' + FIO[1].charAt(0).toUpperCase() + '.');}
 }
